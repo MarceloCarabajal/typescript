6d00c7eabd14cfb1f3282e04a84b9ffe1974cd65
MarceloCarabajal <marce.tsgia@gmail.com>
2024-07-26 14:50:49 -0300

    namespaces

--- main.js
+++ main.js
@@ -1,56 +1,14 @@
 "use strict";
-(() => {
-    let flash = {
-        name: 'Barry',
-        age: 24,
-        powers: ['super velocidad', 'viajar en el tiempo']
+var Validations;
+(function (Validations) {
+    Validations.validateText = (text) => {
+        return (text.length > 3) ? true : false;
     };
-    let superman = {
-        name: 'Clark Kent',
-        age: 60,
-        powers: ['super velocidad'],
-        getNombres() {
-            return this.name;
-        }
+    Validations.validateDate = (myDate) => {
+        return (isNaN(myDate.valueOf()))
+            ? true
+            : false;
     };
-})();
-(() => {
-    class Mutant {
-        mutantPower(id) {
-            return this.name + " " + this.realName;
-        }
-    }
-})();
-(() => {
-    const client = {
-        name: 'Marcelo',
-        age: 33,
-        address: {
-            id: 125,
-            zip: 'KY2 SUD',
-            city: 'Ottawa'
-        },
-        getFullAddress(id) {
-            return this.address.city;
-        }
-    };
-    const client2 = {
-        name: 'Franco',
-        age: 27,
-        address: {
-            id: 127,
-            zip: 'ON1 BOR',
-            city: 'Toronto'
-        },
-        getFullAddress() {
-            return this.name;
-        }
-    };
-})();
-(() => {
-    let addNumbersFunction;
-    addNumbersFunction = (a, b) => {
-        return 10;
-    };
-})();
+})(Validations || (Validations = {}));
+console.log(Validations.validateText('Mar'));
 //# sourceMappingURL=main.js.map
